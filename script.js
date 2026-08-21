8f452a8f53f604d89229a8629f1603bbb65a4537
mionkaKeen <mkane@saac.qld.edu.au>
2026-08-21 11:59:29 +1000

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // Supabase Connection
 // =========================
 
-const SUPABASE_URL = "https://xwadebhflihczxlhjrpg.supabase.co";
+const SUPABASE_URL = "https://cesffaiaxrwoomuttlza.supabase.co/rest/v1/";
 const SUPABASE_KEY = "sb_publishable_4eu0Bs4poL4gOiMoVFAgKA_yp7w_c83";
 
 const sb = window.supabase.createClient(
