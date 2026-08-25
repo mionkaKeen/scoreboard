890baaba473a0dbba23825cf609dfd5a37bc54f9
mionkaKeen <mkane@saac.qld.edu.au>
2026-08-26 08:48:15 +1000

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,34 @@
+javascript
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.17.1/firebase-app.js";
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.17.1/firebase-firestore.js";
+
+
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+
+
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+export { app, auth, db };
+
+console.log("Firebase connected");
+
