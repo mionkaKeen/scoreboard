ac6e078fa71bf34c44d7550e86e80ff22d9d05a4
mionkaKeen <mkane@saac.qld.edu.au>
2026-08-26 08:53:43 +1000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -12,12 +12,13 @@ import {
 
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.firebasestorage.app",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyAxeuvh24T3ZJkV2x-QcNxh8QritKqWqZE",
+  authDomain: "livescoreboard-5194b.firebaseapp.com",
+  projectId: "livescoreboard-5194b",
+  storageBucket: "livescoreboard-5194b.firebasestorage.app",
+  messagingSenderId: "555090260425",
+  appId: "1:555090260425:web:23563595fe0b7a94b5b818",
+  measurementId: "G-ZW34VNG8LB"
 };
 
 const app = initializeApp(firebaseConfig);
